267bcdb30520e3245bc72176d7ae732fa62ed97f
michiburro14@gmail.com <michiburro14@gmail.com>
2020-12-08 15:08:09 +0100

    Add project files.

---
+++ DivN/LibreriaDivisori.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Gestione
+{
+    public class DivisoriN
+    {
+        public static int LeggiN()
+        {
+            Console.Write("inserisci un numero intero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            return n;
+        }
+        public static void StampaDivN(int a)
+        {
+            int i = 1;
+            int max = a / 2;
+
+            while (i <= max)
+            {
+                if (a % i == 0)
+                    Console.WriteLine(i);
+                i++;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ DivN/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using Gestione;
+
+namespace DivN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = DivisoriN.LeggiN();
+            DivisoriN.StampaDivN(a);
+
+        }
+    }
+}
